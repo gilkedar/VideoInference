af245a8db8792503b852e85527003685fbfae236
Gil Kedar <gilkedar1@gmail.com>
2020-05-17 16:17:03 +0300

    added folders and relevant files

---
+++ Client/MainClient.py
@@ -0,0 +1,22 @@
+
+class MainClient:
+
+    def __init__(self):
+        self.setConfigurations()
+        self.initializeResources()
+
+    def setConfigurations(self):
+        pass
+
+    def initializeResources(self):
+        pass
+
+    def run(self):
+
+        pass
+
+
+if __name__ == "__main__":
+    main_client = MainClient()
+    main_client.run()
+
---
+++ Server/MainServer.py
@@ -0,0 +1,22 @@
+
+class MainServer:
+
+    def __init__(self):
+        self.setConfigurations()
+        self.initializeResources()
+
+    def setConfigurations(self):
+        pass
+
+    def initializeResources(self):
+        pass
+
+    def run(self):
+
+        pass
+
+
+if __name__ == "__main__":
+    main_server = MainServer()
+    main_server.run()
+
