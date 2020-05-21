0ef317f73ed44c7eaa48f6874d11b718988a67b0
Gil Kedar <gilkedar1@gmail.com>
2020-05-21 17:00:41 +0300

    * Changed video player to Client's VideoFrameExtractor.py and requestsManager's FrameEditor.py * added condition variable to communicate with RequestsManager when all requests are fulfilled * Client will run in loop until video file ended or until 10 frames are published(for debug) * Added close resources to MainClient

--- Client/MainClient.py
+++ Client/MainClient.py
@@ -1,13 +1,13 @@
 import argparse
 import os
-
+import threading
 from Utils.Settings import Config
 from Utils.Exceptions.Errors import ErrorInvalidInputVideoPath
 from Utils.Exceptions.Errors import ErrorInvalidInputAlgorithmChoice
 from Utils.Exceptions.Errors import ErrorInvalidProtocolChoice
 
 from Client.RequestsManager import RequestsManager
-from Client.VideoPlayer import VideoPlayer
+from Client.VideoFrameExtractor import VideoFrameExtractor
 
 from Utils.Infrastructure.ImageProtocols.ZMQ.ZmqImagePublisher import ZmqImagePublisher
 from Utils.Infrastructure.ImageProtocols.HTTP.HttpImagePublisher import HttpImagePublisher
@@ -18,7 +18,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-a", "--algorithm", required=True, type=str,
                 help="Desired deep Learning algorithm to get inference from cloud resources")
 
-ap.add_argument("-f", "--videofile", required=False,type=str, default=None,
+ap.add_argument("-f", "--videofile", required=False,type=str,
                 help="Full Path to desired video file. If no path chosen, read from camera driver")
 
 ap.add_argument("-i", "--ip", required=False, type=str, default=Config.LOCALHOST_IP,
@@ -41,22 +41,38 @@ class MainClient:
         self.video_file_path = None
 
         self.requests_manager = None
-        self.video_player = None
+        self.video_frame_extractor = None
         self.requests_publisher = None
 
+        self.condition_received_all_requests = threading.Condition()
+
+    def initResources(self):
+        self.setInputParams(self.input_params)
         # self.readServerParameters() @TODO - add routine to read info from DB
-        self.setInputParams(input_params)
-        self.initResources()
+        self.requests_manager = RequestsManager(self.desired_algorithm,self.condition_received_all_requests)
+        self.video_frame_extractor = VideoFrameExtractor(self.video_file_path)
         self.initImagePublisher()
+
+    def closeResources(self):
+        self.requests_manager.closeResources()
+        self.requests_manager = None
+        self.requests_publisher = None
+        self.video_frame_extractor.closeSource()
+        self.video_frame_extractor = None
+
+    def getAlgorithmName(self):
+        return self.desired_algorithm
 
     def setInputParams(self,params):
         algorithm_name = params[Config.INPUT_PARAM_ALGORITHM_NAME]
+        video_file_path = params[Config.INPUT_PARAM_VIDEO_FILE_NAME]
 
         # validate video path
         if Config.INPUT_PARAM_VIDEO_FILE_NAME in params:
-            self.video_file_path = params[Config.INPUT_PARAM_VIDEO_FILE_NAME]
-            if not os.path.exists(self.video_file_path):
-                raise ErrorInvalidInputVideoPath(self.video_file_path)
+            if video_file_path:
+                self.video_file_path = params[Config.INPUT_PARAM_VIDEO_FILE_NAME]
+                if not os.path.exists(self.video_file_path):
+                    raise ErrorInvalidInputVideoPath(self.video_file_path)
 
         # validate algorithm name
         if algorithm_name != Config.ALGORITHM_IS_SANTA:
@@ -76,10 +92,6 @@ class MainClient:
             self.image_protocol = params[Config.INPUT_PARAM_PROTOCOL_NAME]
 
 
-    def initResources(self):
-        self.requests_manager = RequestsManager(self.desired_algorithm)
-        self.video_player = VideoPlayer(self.video_file_path)
-
     def initImagePublisher(self):
         if self.image_protocol == Config.PROTOCOL_ZMQ:
             self.requests_publisher = ZmqImagePublisher(self.server_ip)
@@ -88,22 +100,41 @@ class MainClient:
         else:
             raise ErrorInvalidProtocolChoice(self.image_protocol)
 
+    def publishRequest(self,req_msg):
+        self.requests_publisher.publish(req_msg)
+
+    def notifyEndOfFrames(self):
+        print("Finished sending all frames")
+        self.requests_manager.setEndOfFrames()
+
     def run(self):
-        # Open video source
-        self.video_player.openSource()
+        # initialize client resources
+        self.initResources()
+        # start listening
         self.requests_manager.startListeningToIncomingResponses()
 
-        while not self.video_player.finished:
+        while True:
             # Read frame by frame
-            img = self.video_player.getNextFrame()
-            # generate relevant request
-            req_msg = self.requests_manager.generateRequest(img)
-            # publish request
-            self.requests_publisher.publish(req_msg)
+            frame_id, frame = self.video_frame_extractor.getNextFrame()
 
-        pass
+            if self.video_frame_extractor.finished:
+                break
+            # generate relevant request
+            req_msg = self.requests_manager.generateRequestMessage(frame_id,frame)
+            # publish request
+            self.publishRequest(req_msg)
+
+            if frame_id == 10: # for debugging purposes
+                break
+
+        self.notifyEndOfFrames()
+        print("Waiting for all requests")
+        with self.condition_received_all_requests:
+            self.condition_received_all_requests.wait()
+        print("Finished all requests")
 
 
 if __name__ == "__main__":
     main_client = MainClient(input_arguments)
     main_client.run()
+    main_client.closeResources()
